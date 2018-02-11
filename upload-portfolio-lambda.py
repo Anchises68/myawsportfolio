23010a619cb3f576e919a63131cc6806ce361aa8
Alex Mireles <anchises1968@gmail.com>
2018-02-11 15:11:30 -0800

    added failure notice to lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -8,22 +8,23 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:822374759106:deployMirelesCloudTopic')
 
+    try:
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('mirelescloud.com')
+        build_bucket = s3.Bucket('portfoliobuild.mirelescloud.com')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-    s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('mirelescloud.com')
-    build_bucket = s3.Bucket('portfoliobuild.mirelescloud.com')
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
-    print "Great Success"
-    topic.publish(Subject="MirelesCloud Portfolio", Message="Great Success")
+        print "Great Success"
+        topic.publish(Subject="MirelesCloud Portfolio", Message="Great Success")
+    except:
+        topic.publish(Subject="Error - MirelesCloud Portfolio", Message="Failure")
 
     # TODO implement
     return 'Hello from Lambda'
