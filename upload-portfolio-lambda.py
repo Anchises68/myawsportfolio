df1d07bf9435b3e648d4ecc16d04b8d1b93e3ff9
Alex Mireles <anchises1968@gmail.com>
2018-02-11 20:28:19 -0800

    completed lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -8,12 +8,25 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:822374759106:deployMirelesCloudTopic')
 
+    location = {
+        "bucketName": 'portfoliobuild.mirelescloud.com',
+        "objectKey": 'portfoliobuild.zip'
+    }
+
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["name"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+         print "Building portoflio from " + str(location)
         s3 = boto3.resource('s3')
         portfolio_bucket = s3.Bucket('mirelescloud.com')
-        build_bucket = s3.Bucket('portfoliobuild.mirelescloud.com')
+        build_bucket = s3.Bucket(location["bucketName"])
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -23,10 +36,12 @@ def lambda_handler(event, context):
 
         print "Great Success"
         topic.publish(Subject="MirelesCloud Portfolio", Message="Great Success")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject="Error - MirelesCloud Portfolio", Message="Failure")
         raise
-    
 
     # TODO implement
     return 'Hello from Lambda'
