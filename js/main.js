d808cc4312006f52426641e3362612151f903638
Alex Mireles <anchises1968@gmail.com>
2018-03-15 13:01:10 -0700

    index.html updates

--- js/main.js
+++ js/main.js
@@ -33,6 +33,7 @@ const myWork = [
       'comment': `Bengal cat” by roberto shabs is licensed under CC BY 2.0
                   https://www.flickr.com/photos/37287295@N00/2540855181`
     }
+    
   },
 ]
 ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
